2cedff2eab04d8d0d10f82310dc0cd62b04badd1
Yonggang Luo <luoyonggang@gmail.com>
2023-02-08 16:05:41 +0800

    upgrade to 2023 msys2 release

--- scritps/mingw-setup.ps1
+++ scritps/mingw-setup.ps1
@@ -1,6 +1,6 @@
 $env:MSYS = "winsymlinks:nativestrict"
 $env:MSYS2_FINGERPRINT = "0"
-$env:MSYS2_URL = "https://github.com/msys2/msys2-installer/releases/download/2022-09-04/msys2-base-x86_64-20220904.sfx.exe"
+$env:MSYS2_URL = "https://github.com/msys2/msys2-installer/releases/download/2023-01-27/msys2-base-x86_64-20230127.sfx.exe"
 
 $env:MSYSTEM="MINGW32"
 & "$PSScriptRoot\msys2-prepare.ps1"
