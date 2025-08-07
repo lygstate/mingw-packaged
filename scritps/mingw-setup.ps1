5e38188fc6a7bcbe7d4b2a43bca354791ba8b6a9
Yonggang Luo <luoyonggang@gmail.com>
2025-08-07 15:33:58 +0800

    mingw32 is not working anymore

--- scritps/mingw-setup.ps1
+++ scritps/mingw-setup.ps1
@@ -5,11 +5,11 @@ $env:MSYS2_URL = "https://github.com/msys2/msys2-installer/releases/download/202
 $env:MSYS2_PACKAGES = ""
 $env:MESA_PACKAGES = ""
 
-$env:MSYSTEM="MINGW32"
-& "$PSScriptRoot\msys2-prepare.ps1"
-& "$PSScriptRoot\qemu-packages.ps1"
-& "$PSScriptRoot\mesa-packages.ps1"
-& "$PSScriptRoot\extra-packages.ps1"
+#$env:MSYSTEM="MINGW32"
+#& "$PSScriptRoot\msys2-prepare.ps1"
+#& "$PSScriptRoot\qemu-packages.ps1"
+#& "$PSScriptRoot\mesa-packages.ps1"
+#& "$PSScriptRoot\extra-packages.ps1"
 
 $env:MSYSTEM="MINGW64"
 & "$PSScriptRoot\msys2-prepare.ps1"
